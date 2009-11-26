90a9ddc306df77a9793970bddf32346205d7dff3
nate <nate@12b94269-fd21-4610-a088-bc799811b733>
2009-11-26 12:19:30 +0000

    - Added KeyTester
    git-svn-id: http://svn.nate.deepcreek.org.au/svn/KeyboardRedirector/trunk@28 12b94269-fd21-4610-a088-bc799811b733

---
+++ KeyTester/Form1.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace KeyTester
+{
+    partial class KeyTesterForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBoxEvents = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // richTextBoxEvents
+            // 
+            this.richTextBoxEvents.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBoxEvents.Location = new System.Drawing.Point(12, 12);
+            this.richTextBoxEvents.Name = "richTextBoxEvents";
+            this.richTextBoxEvents.Size = new System.Drawing.Size(321, 258);
+            this.richTextBoxEvents.TabIndex = 0;
+            this.richTextBoxEvents.Text = "";
+            // 
+            // KeyTesterForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(345, 310);
+            this.Controls.Add(this.richTextBoxEvents);
+            this.KeyPreview = true;
+            this.Name = "KeyTesterForm";
+            this.Text = "Key Tester";
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.KeyTesterForm_KeyUp);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.KeyTesterForm_KeyDown);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBoxEvents;
+    }
+}
+
---
+++ KeyTester/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace KeyTester
+{
+    public partial class KeyTesterForm : Form
+    {
+        public KeyTesterForm()
+        {
+            InitializeComponent();
+        }
+
+        private void KeyTesterForm_KeyDown(object sender, KeyEventArgs e)
+        {
+            richTextBoxEvents.AppendText("KeyDown\t" + e.KeyCode.ToString() + Environment.NewLine);
+            e.Handled = true;
+        }
+
+        private void KeyTesterForm_KeyUp(object sender, KeyEventArgs e)
+        {
+            richTextBoxEvents.AppendText("KeyUp\t" + e.KeyCode.ToString() + Environment.NewLine);
+            e.Handled = true;
+        }
+
+    }
+}
